9d774dc386041fcfa640c046a16a155c20f5c5b3
scanf-ak27 <kraayush27@gmail.com>
2023-09-27 21:03:43 +0530

    7th

--- src/components/home/logo/index.js
+++ src/components/home/logo/index.js
@@ -35,6 +35,26 @@ const Logo=()=>{
         
 
     },[])
+    return(
+
+        <div className='logo-container' ref={bgRef}>
+            <img className="solid-logo" ref={solidLogoRef} src={LogoS} alt="Javascript, Developer"/>
+            <svg
+        width="559pt"
+        height="897pt"
+        version="1.0"
+        viewBox="0 0 559 897"
+        xmlns="http://www.w3.org/2000/svg"
+       >
+        <g
+          className="svg-container"
+          transform="translate(0 457) scale(.1 -.1)"
+          fill="none"
+        >
+        </g>
+      </svg>
+   </div>
+    )
 }
 
 export default Logo
