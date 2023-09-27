9c4eed5630120f84b0be9118652bf1c0b6dd254a
scanf-ak27 <kraayush27@gmail.com>
2023-09-27 19:40:22 +0530

    5th

--- src/components/home/logo/index.js
+++ src/components/home/logo/index.js
@@ -1,40 +1,40 @@
-import './index.scss'
-import LogoS from '../../../assets/images/logo-s.png'
-import { useEffect, useRef } from 'react'
-import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
-const Logo=()=>{
+// import './index.scss'
+// import LogoS from '../../../assets/images/logo-s.png'
+// import { useEffect, useRef } from 'react'
+// import gsap from 'gsap-trial'
+// import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+// const Logo=()=>{
 
-    const bgRef=useRef();
-    const outlineLogoRef=useRef();
-    const solidLogoRef=useRef();
+//     const bgRef=useRef();
+//     const outlineLogoRef=useRef();
+//     const solidLogoRef=useRef();
 
-    useEffect(()=> {
-        gsap.registerPlugin(DrawSVGPlugin)
+//     useEffect(()=> {
+//         gsap.registerPlugin(DrawSVGPlugin)
 
-        gsap.timeline()
-        .to(bgRef.current,{
-            duration:1,
-            opacity:1
-        })
-        .from(outlineLogoRef.current,{
-            drawSCG:0,
-            duration:20,
-        })
-        gsap.fromTo(
-            solidLogoRef.current,
-            {
-              opacity: 0,
-            },
-            {
-              opacity: 1,
-              delay: 4,
-              duration: 4,
-            }
-          )
+//         gsap.timeline()
+//         .to(bgRef.current,{
+//             duration:1,
+//             opacity:1
+//         })
+//         .from(outlineLogoRef.current,{
+//             drawSCG:0,
+//             duration:20,
+//         })
+//         gsap.fromTo(
+//             solidLogoRef.current,
+//             {
+//               opacity: 0,
+//             },
+//             {
+//               opacity: 1,
+//               delay: 4,
+//               duration: 4,
+//             }
+//           )
         
 
-    },[])
-}
+//     },[])
+// }
 
-export default Logo
+// export default Logo
